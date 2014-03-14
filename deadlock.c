e6d2451db53db276df24d308b02b6bac7b68dc79
Sai <jiangyaoxuan@gmail.com>
2014-03-13 20:07:11 -0400

    add goto

--- deadlock.c
+++ deadlock.c
@@ -24,9 +24,9 @@ pthread_mutex_t critical_mutex = PTHREAD_MUTEX_INITIALIZER;
 void input();
 void *commandGenerator(void *id);
 void release(int pid);
-void bankerAllocation();
-void detectionAllocation();
-void (*allocation)();
+void bankerAllocation(int pid);
+void detectionAllocation(int pid);
+void (*allocation)(int);
 
 
 int main(int argc, char *argv[]){
@@ -51,26 +51,26 @@ int main(int argc, char *argv[]){
 	 	allocation = &detectionAllocation;
 	 }
 
-	 max = malloc(sizeof(* int) * no_proc);
+	 max = malloc(sizeof(int *) * no_proc);
 	 for (i = 0; i < no_proc; ++i){		
 	 	max[i] = malloc(no_res * sizeof(int));
-	 	for (j = 0; j < count; ++j){
+	 	for (j = 0; j < no_res; ++j){
 	 		max[i][j] = rand()%range_res;
 	 	}
 	 }
 
-	 need = malloc(sizeof(* int) * no_proc);
+	 need = malloc(sizeof(int *) * no_proc);
 	 for (i = 0; i < no_proc; ++i){		
 	 	need[i] = malloc(no_res * sizeof(int));
-	 	for (j = 0; j < count; ++j){
+	 	for (j = 0; j < no_res; ++j){
 	 		need[i][j] = max[i][j];
 	 	}
 	 }
 
-	 hold = malloc(sizeof(* int) * no_proc);
+	 hold = malloc(sizeof(int *) * no_proc);
 	 for (i = 0; i < no_proc; ++i){		
 	 	hold[i] = malloc(no_res * sizeof(int));
-	 	for (j = 0; j < count; ++j){
+	 	for (j = 0; j < no_res; ++j){
 	 		hold[i][j] = 0;
 	 	}
 	 }
@@ -104,22 +104,23 @@ void input(){
 
 void *commandGenerator(void *id){
 	int p_id = *((int *)id);
-
 	int is_release = rand()%2;
 
-	if (is_release){
-		release(p_id);
-	}else{
-
-	}
-
-	while(true){
-
+	
+	while(1){
+		
+		if (is_release){
+			release(p_id);
+		}else{
+			allocation(p_id);
+		}
 	}
 }
 
 void release(int pid){
 	pthread_mutex_lock(&critical_mutex);	
+
+	printf("is releasing proc %d\n", pid);
 
 	int index = rand()%no_res;
 	int i;
@@ -127,9 +128,39 @@ void release(int pid){
 	for(i = 0;i<no_res;i++){
 		if(hold[pid][(index+i)%no_res] != 0){
 			hold[pid][(index+i)%no_res] = 0;
+			printf("is releasing res %d from proc %d, total \n", (index+i)%no_res, pid,hold[pid][(index+i)%no_res]);
 			break;
 		}
 	}
+
+	pthread_mutex_unlock(&critical_mutex);
+}
+
+void bankerAllocation(int pid){
+	pthread_mutex_lock(&critical_mutex);
+
+	int request_no = rand()%range_res;
+	int request_res = rand()%no_res;
+	
+	step_1:
+	if (request_no > need[pid][request_res])
+	{
+		printf("error, impossible request amount\n");
+		goto banker_end;
+	}
+
+	step_2:
+	if (request_no > avail[request_res])
+	{
+		/* code */
+	}
+
+	banker_end:
+		pthread_mutex_unlock(&critical_mutex);
+}
+
+void detectionAllocation(int pid){
+	pthread_mutex_lock(&critical_mutex);
 
 	pthread_mutex_unlock(&critical_mutex);
 }
